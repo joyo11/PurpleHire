a6c573fcc42295ce58daa97c1261c11c8248b64f
Mohammad Shafay Joyo <shafay11august@gmail.com>
2026-03-26 20:35:26 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   devIndicators: false,
   images: {
     domains: [], // Add any external image domains if needed
